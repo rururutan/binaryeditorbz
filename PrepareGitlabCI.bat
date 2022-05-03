3f1c7faeb1a8ff59ae1336691769a5d8cb6a2468
devil.tamachan <devil.tamachan@gmail.com>
2022-05-03 23:31:55 +0900

    [修正] gitlab-ci

--- PrepareGitlabCI.bat
+++ PrepareGitlabCI.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools" --add Microsoft.VisualStudio.Component.VC.ATL --downloadThenInstall
+"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools" --add Microsoft.VisualStudio.Component.VC.ATL --downloadThenInstall --quiet --norestart
